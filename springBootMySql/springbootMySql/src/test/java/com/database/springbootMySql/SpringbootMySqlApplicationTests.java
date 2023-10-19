f75e5b572f5827122141a8819dc7eac08b7b095a
Damith Anushka <damithanushkam016@gmail.com>
2023-10-19 22:25:31 +0530

    add changres

---
+++ springBootMySql/springbootMySql/src/test/java/com/database/springbootMySql/SpringbootMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.springbootMySql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
